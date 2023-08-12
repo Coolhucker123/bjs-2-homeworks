5d55330b743fefc59a9e9f8a4f967054cef0f021
Александр Толмачёв <tolmachev96.96@mail.ru>
2023-08-12 14:37:26 +0700

    welldone

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,51 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
   
-}
+    function wrapper(...args) {
+        const hash = md5(args.join(","));
+        let objectInCache = cache.find((item) => item.hash === hash)
+        if(objectInCache) {
+            console.log("Из кеша: " + objectInCache.result);
+            return "Из кеша: " + objectInCache.result;
+        }
+  
+        const result = func(...args);
+        cache.push({hash, result});
+        if(cache.length > 5) {
+            cache.shift()
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
+  };
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+    
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if(timeoutId) {
+          console.log("удалили текущий таймаут");
+        clearTimeout(timeoutId);
+        }
+        else {
+            wrapper.count++;
+            func(...args)
+        }
   
-}
+        console.log("создаём новый таймаут");
+        timeoutId = setTimeout(() => {
+          timeoutId = null;
+          func(...args);
+          console.log("вызвали колбек");
+          }, delay);
+    }
+  
+    return wrapper;
+  };
+
